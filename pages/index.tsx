3de36561e903bafcd98dad61fdf9dee148719ad4
Nick Valdez <nicholastvaldez@gmail.com>
2023-06-30 15:21:15 -0500

    initial commit and project setup

---
+++ pages/index.tsx
@@ -0,0 +1,26 @@
+import Image from "next/image"
+import { Inter } from "next/font/google"
+
+const inter = Inter({ subsets: ["latin"] })
+
+export default function Home() {
+  return (
+    <div>
+      <title>Nick Valdez's Portfolio</title>
+
+      {/* Header */}
+
+      {/* Hero */}
+
+      {/* About */}
+
+      {/* Experience */}
+
+      {/* Skills */}
+
+      {/* Projects */}
+
+      {/* Contact Me */}
+    </div>
+  )
+}
