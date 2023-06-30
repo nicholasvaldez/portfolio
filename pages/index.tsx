26997b2484a2b2bd06241264dca7bf42b7617bb9
Nick Valdez <nicholastvaldez@gmail.com>
2023-06-30 16:13:35 -0500

    install typewriter and basic congig

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image"
 import { Inter } from "next/font/google"
 import Header from "@/components/Header"
+import Hero from "@/components/Hero"
 
 const inter = Inter({ subsets: ["latin"] })
 
@@ -8,11 +9,12 @@ export default function Home() {
   return (
     <div>
       <title>Nick Valdez's Portfolio</title>
-
-      {/* Header */}
       <Header />
 
       {/* Hero */}
+      <section id="hero">
+        <Hero />
+      </section>
 
       {/* About */}
 
