2148f3f7ce6c8424541e1487e3582eb1dd741d6f
Nick Valdez <nicholastvaldez@gmail.com>
2023-07-02 17:58:04 -0500

    projects section setup and ready for data

---
+++ components/Projects.tsx
@@ -0,0 +1,47 @@
+import { motion } from "framer-motion"
+type Props = {}
+
+function Projects({}: Props) {
+  const projects = [1, 2, 3]
+  return (
+    <motion.div
+      initial={{ opacity: 0 }}
+      whileInView={{ opacity: 1 }}
+      transition={{ duration: 1.5 }}
+      className="h-screen relative flex overflow-hidden flex-col text-left md:flex-row max-w-full justify-evenly mx-auto items-center z-0"
+    >
+      <h3 className="absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl ">
+        Projects
+      </h3>
+
+      <div className="relative w-full flex overflow-x-scroll overflow-y-hidden snap-x snap-mandatory z-20  ">
+        {projects.map((p) => (
+          <div className="w-screen flex-shrink-0 snap-center flex flex-col space-y-5 items-center justify-center p-20 md:p-44 h-screen">
+            <motion.img
+              initial={{
+                y: -300,
+                opacity: 0,
+              }}
+              transition={{ duration: 1.2 }}
+              whileInView={{ opacity: 1, y: 0 }}
+              viewport={{ once: true }}
+              src="https://img-prod-cms-rt-microsoft-com.akamaized.net/cms/api/am/imageFileData/RE4LJcl?ver=3fd0&q=90&m=6&h=705&w=1253&b=%23FFFFFFFF&f=jpg&o=f&p=140&aim=true"
+              alt=""
+              className="xl:w-[600px]"
+            />
+            <div className="space-y-10 px-0 md:px-10 max-w-6xl text-white">
+              <h4 className="text-4xl font-semibold text-center">SamplStak</h4>
+              <p className="text-large text-center md:text-left">
+                Project summary goes here
+              </p>
+            </div>
+          </div>
+        ))}
+      </div>
+
+      <div className="w-full absolute top-[30%] bg-[#F6D0B1]/20 left-0 h-[500px] -skew-y-12 "></div>
+    </motion.div>
+  )
+}
+
+export default Projects
