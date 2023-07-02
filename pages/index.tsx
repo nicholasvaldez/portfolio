70f05fb8f690ed434b33d58d49b46293854bb378
Nick Valdez <nicholastvaldez@gmail.com>
2023-07-02 18:59:52 -0500

    completed contact form

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,6 +5,7 @@ import About from "@/components/About"
 import WorkExperience from "@/components/WorkExperience"
 import Skills from "@/components/Skills"
 import Projects from "@/components/Projects"
+import ContactMe from "@/components/ContactMe"
 
 const inter = Inter({ subsets: ["latin"] })
 
@@ -32,6 +33,9 @@ export default function Home() {
       </section>
 
       {/* Contact Me */}
+      <section id="contact" className="snap-start">
+        <ContactMe />
+      </section>
     </div>
   )
 }
