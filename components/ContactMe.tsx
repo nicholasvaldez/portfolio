d16ea9ba06a3acbab8c4638a659eeba9ef344a00
Nick Valdez <nicholastvaldez@gmail.com>
2023-07-03 07:23:25 -0500

    setup and intall sanity.io

--- components/ContactMe.tsx
+++ components/ContactMe.tsx
@@ -21,8 +21,8 @@ function ContactMe({}: Props) {
       <h3 className="absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl ">
         Contact
       </h3>
-      <div className="flex flex-col space-y-10">
-        <h4 className="text-4xl font-semibold text-center ">
+      <div className="flex flex-col space-y-10 md:h-[500px]">
+        <h4 className="text-3xl font-semibold text-center ">
           I'm just what your team needs.{" "}
           <span className="underline decoration-[#F6D0B1]/50">
             {" "}
--- pages/index.tsx
+++ pages/index.tsx
@@ -6,12 +6,13 @@ import WorkExperience from "@/components/WorkExperience"
 import Skills from "@/components/Skills"
 import Projects from "@/components/Projects"
 import ContactMe from "@/components/ContactMe"
+import Link from "next/link"
 
 const inter = Inter({ subsets: ["latin"] })
 
 export default function Home() {
   return (
-    <div className="bg-[#181E1E] text-[#C0C0C3] h-screen snap-y snap-mandatory overflow-scroll z-0  ">
+    <div className="bg-[#181E1E] text-[#C0C0C3] h-screen snap-y snap-mandatory overflow-y-scroll overflow-x-hidden z-0 scrollbar scrollbar-track-gray-400/20 scrollbar-thumb-[#F6D0B1]/80">
       <title>Nick Valdez's Portfolio</title>
       <Header />
 
@@ -27,12 +28,10 @@ export default function Home() {
         <Skills />
       </section>
 
-      {/* Projects */}
       <section id="projects" className="snap-start">
         <Projects />
       </section>
 
-      {/* Contact Me */}
       <section id="contact" className="snap-start">
         <ContactMe />
       </section>
