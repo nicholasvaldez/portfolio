d8cc8a01a63c45ef26fc69024546e46365d30ccc
Nick Valdez <nicholastvaldez@gmail.com>
2023-07-06 14:32:47 -0500

    updated link

--- components/Skills.tsx
+++ components/Skills.tsx
@@ -32,6 +32,7 @@ function Skills({ skills }: Props) {
         <Skill url="https://raw.githubusercontent.com/devicons/devicon/1119b9f84c0290e0f0b38982099a2bd027a48bf1/icons/pytest/pytest-original-wordmark.svg" />
         <Skill url="https://raw.githubusercontent.com/devicons/devicon/1119b9f84c0290e0f0b38982099a2bd027a48bf1/icons/vscode/vscode-original.svg" />
       </div>
+      //test
     </motion.div>
   )
 }
