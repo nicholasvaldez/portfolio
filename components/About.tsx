a30892c1efab4042c2c78a24b264f0ae152e2dee
Nick Valdez <nicholastvaldez@gmail.com>
2023-07-26 21:52:48 -0500

    About responsiveness

--- components/About.tsx
+++ components/About.tsx
@@ -8,7 +8,7 @@ type Props = {
 
 function About({ pageInfo }: Props) {
   return (
-    <div className="flex flex-col relative h-screen text-center md:text-left md:flex-row max-w-7xl px-10 justify-evenly mx-auto items-center">
+    <div className="flex flex-col relative h-screen  md:py-0 text-center md:text-left md:flex-row max-w-7xl px-10 justify-evenly mx-auto items-center">
       <h3 className="absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl ">
         About
       </h3>
@@ -24,16 +24,16 @@ function About({ pageInfo }: Props) {
         whileInView={{ opacity: 1, x: 0 }}
         viewport={{ once: true }}
         src={urlFor(pageInfo?.profilePic).url()}
-        className="-mb-20 md:mb-0 flex-shrink-0 w-56 h-56 rounded-full object-cover md:rounded-lg md:w-64 md:h-96 xl:w-[400px] xl:h-[500px]"
+        className="-mb-44 md:-mb-10 flex-shrink-0 w-40 h-40 rounded-full object-cover md:rounded-lg md:w-64 md:h-96 xl:w-[400px] xl:h-[500px]"
       />
 
-      <div className="space-y-5 md:space-y-10 sm:w-auto relative md:static w-full">
+      <div className="space-y-5 pb-32 md:pb-0 md:space-y-10 sm:w-auto relative md:static w-full">
         <h4 className="text-4xl font-semibold">
           Here's a{" "}
           <span className="underline decoration-[#F6D0B1]/50">little</span>{" "}
           background
         </h4>
-        <p className="text-base w-[100%] h-[190%] sm:h-[100px] md:w-[500px] md:h-[320px] lg:w-[700px] lg:h-[430px] overflow-y-scroll absolute md:static scrollbar scrollbar-track-gray-400/20 md:scrollbar-track-transparent scrollbar-thumb-[#F6D0B1]/80 ">
+        <p className="text-base w-[100%] h-72  md:w-[500px] md:h-[320px] lg:w-[700px] lg:h-[430px] overflow-y-scroll absolute md:static scrollbar scrollbar-track-gray-400/20 md:scrollbar-track-transparent scrollbar-thumb-[#F6D0B1]/80 ">
           {pageInfo.backgroundInformation}
         </p>
       </div>
